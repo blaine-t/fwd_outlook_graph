e72252cd2f02da14e92aa5316b2ea2f58fcd1eea
Blaine <blaine-t@bathost.net>
2023-07-28 14:36:36 -0500

    Added clientState, resubscribe, unsubscribe

--- fwd_outlook_graph/main.py
+++ fwd_outlook_graph/main.py
@@ -1,72 +1,87 @@
 #!/usr/bin/env python3
 
-"""
-The configuration file (default config.json) would look like this:
-{
-    "authority": "https://login.microsoftonline.com/common",
-    "client_id": "your_client_id",
-}
-    # You can find the other permission names from this document
-    # https://docs.microsoft.com/en-us/graph/permissions-reference
-    # To restrict who can login to this app, you can find more Microsoft Graph API endpoints from Graph Explorer
-    # https://developer.microsoft.com/en-us/graph/graph-explorer
-
-You can then run this script:
-    ./main.py
-"""
+# Other script imports
 
 from auth import get_access_token
 from cache import get_cache
 from config import load_config
-from request import forward_email, subscribe
-
-config_file_path = "config.json"  # Update the file path if the config file is in a different location
-cache_file_path = "token_cache.bin" # Update the file path if the cache is in a different location
-
-# Optional logging
-# logging.basicConfig(level=logging.DEBUG)
+from request import forward_email, subscribe, resubscribe, unsubscribe
 
 import flask
+import logging
+
+# Update the file path if the cache or config file is in a different location
+cache_file_path = "token_cache.bin"
+config_file_path = "config.json"
+
+logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
 
 # Initialize Flask server
 app = flask.Flask(__name__)
 
+# Initialize cache and config
+cache = get_cache(cache_file_path)
+config = load_config(config_file_path)
+
 # Callback endpoint to process incoming notifications
 @app.route("/sub", methods=["POST"])
-def handle_notification():
-    print("Got notif!")
-    print(flask.request.content_type)
+def handle_sub_post():
+    # Subscription verification confirmation
     if "text/plain" in flask.request.content_type:
-        # Respond to Microsoft's validation request during webhook setup
-        print(flask.request.args['validationToken'])
         response = app.response_class(
                 response=flask.request.args['validationToken'],
                 status=200,
                 mimetype='text/plain'
             )
         return response
+    # Handle subscription event
     elif 'application/json' in flask.request.content_type:
         notification = flask.request.get_json()
-        # Handle the new message notification here
+        print(notification)
+        # Read the notification
         if notification["value"]:
+            # Access the list in the notification
             for item in notification["value"]:
-                if item["resourceData"]["@odata.type"] == "#Microsoft.Graph.Message":
-                    # Retrieve the message ID from the notification
-                    message_id = item["resourceData"]["id"]
-                    config = load_config(config_file_path)
-                    cache = get_cache(cache_file_path)
-                    forward_email(get_access_token(cache, config), message_id, config["to_recipients"])
-                    # Now you can fetch the message content using the message_id and forward the email
+                # Check to make sure the client state matches the one provided
+                if item["clientState"] == config["client_state"]:
+                    if item["changeType"] == "created":
+                        if item["resourceData"]["@odata.type"] == "#Microsoft.Graph.Message":
+                            # Retrieve the message ID from the notification
+                            message_id = item["resourceData"]["id"]
+                            # Now you can fetch the message content using the message_id and forward the email
+                            forward_email(get_access_token(cache, config), message_id, config["to_recipients"])
+                            # Acknowledge receipt of the notification
+                            return "", 204
+                        else:
+                            print("Unexpected odata.type")
+                            print(f'odata.type: {item["resourceData"]["@odata.type"]}')
+                    elif item["changeType"] == "reauthorizationRequired":
+                        resubscribe()
+                        # Acknowledge receipt of the notification
+                        return "", 204
+                    else:
+                        print ("Unknown changeType")
+                        print(f'changeType: {item["changeType"]}')
+                else:
+                    print("Mismatched clientState")
+                    print(f'clientState: {item["clientState"]}')
+                print(f'Notification: {notification}')
+                return "", 501
 
-    return "", 204  # Acknowledge receipt of the notification
+@app.route("/sub", methods=["GET"])
+def handle_sub_get():
+    subscribe(get_access_token(cache, config), config)
+    return "", 200
 
-@app.route("/", methods=["GET"])
-def main():
-    config = load_config(config_file_path)
-    cache = get_cache(cache_file_path)
-    access_token = get_access_token(cache, config)
-    subscribe(access_token, config["subscription_url"])
-    # send_email(access_token, config['to_recipients'])
+@app.route("/unsub", methods=["GET"])
+def handle_unsub():
+    unsubscribe(get_access_token(cache, config), flask.request.args['subscriptionId'])
+    return "", 200
+
+@app.route("/resub", methods=["GET"])
+def handle_resub():
+    resubscribe(get_access_token(cache, config), flask.request.args['subscriptionId'])
     return "", 200
 
 if __name__ == '__main__':
--- fwd_outlook_graph/request.py
+++ fwd_outlook_graph/request.py
@@ -9,10 +9,16 @@ def format_recipient(recipient):
         }
     }
 
+def get_time_str():
+    time = datetime.datetime.now() + datetime.timedelta(minutes = 4230)
+    return str(time).replace(" ", "T") + "0Z"
+
+def get_headers(access_token):
+    return {'Authorization': f'Bearer {access_token}'}
+
 def forward_email(access_token, message_id, to_recipients):
     # Calling graph using the access token
     url = f'https://graph.microsoft.com/v1.0/me/messages/{message_id}/forward'
-    headers = {'Authorization': f'Bearer {access_token}', 'Content-Type': 'application/json'}
     json = {
         'Comment': "",
         'ToRecipients': [
@@ -20,20 +26,38 @@ def forward_email(access_token, message_id, to_recipients):
         }
     for recipient in to_recipients:
         json['ToRecipients'].append(format_recipient(recipient))
-    graph_data = requests.post(url, headers=headers, json=json)
-    print("Graph API call result: %s" % graph_data.status_code)
+    graph_data = requests.post(url, headers=get_headers(access_token), json=json)
+    print("Graph API call result: %s" % graph_data.text)
+    print(graph_data.status_code)
 
-def subscribe(access_token, notificationUrl):
+def subscribe(access_token, config):
+    # Calling graph using the access token
     url = "https://graph.microsoft.com/v1.0/subscriptions"
-    headers = { 'Authorization': f'Bearer {access_token}', 'Content-Type': 'application/json' }
-    time = datetime.datetime.now() + datetime.timedelta(minutes = 4230)
-    timeStr = str(time).replace(" ", "T") + "0Z"
+    # Add 4230 minutes to the current time to enable the longest subscription expiry GRAPH allows
     json = {
         "changeType": "created",
-        "notificationUrl": notificationUrl,
+        "notificationUrl": config["subscription_url"],
         "resource": "me/mailFolders('Inbox')/messages",
-        "expirationDateTime": timeStr,
+        "expirationDateTime": get_time_str(),
+        "clientState": config["client_state"]
     }
-    subscribe_data = requests.post(url, headers=headers, json=json)
+    subscribe_data = requests.post(url, headers=get_headers(access_token), json=json)
     print("Subscription call result: %s" % subscribe_data.text)
     print(subscribe_data.status_code)
+
+def resubscribe(access_token, subscription_id):
+    # Calling graph using the access token
+    url = f'https://graph.microsoft.com/v1.0/subscriptions/{subscription_id}'
+    json = {
+        "expirationDateTime": get_time_str()
+    }
+    resubscribe_data = requests.patch(url, headers=get_headers(access_token), json=json)
+    print("Resubscription call result: %s" % resubscribe_data.text)
+    print(resubscribe_data.status_code)
+
+def unsubscribe(access_token, subscription_id):
+    # Calling graph using the access token
+    url = f'https://graph.microsoft.com/v1.0/subscriptions/{subscription_id}'
+    unsubscribe_data = requests.delete(url, headers=get_headers(access_token))
+    print("Unsubscription call result: %s" % unsubscribe_data.text)
+    print(unsubscribe_data.status_code)
